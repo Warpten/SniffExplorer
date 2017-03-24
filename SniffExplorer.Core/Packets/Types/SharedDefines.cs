04082a41e519f89e78c6eafa869d46ec0311655f
Warpten <vertozor@gmail.com>
2017-03-24 06:56:10 -0500

    missing file for previous commit

---
+++ SniffExplorer.Core/Packets/Types/SharedDefines.cs
@@ -0,0 +1,25 @@
+﻿namespace SniffExplorer.Core.Packets.Types
+{
+    public enum PowerType : byte
+    {
+        Mana,
+        Rage,
+        Focus,
+        Energy,
+        ComboPoints,
+        Runes,
+        RunicPower,
+        SoulShards,
+        LunarPower,
+        HolyPower,
+        Alternate,
+        Maelstrom,
+        Chi,
+        Insanity,
+        BurningEmbers,
+        DemonicFury,
+        ArcaneCharges,
+        Fury,
+        Pain
+    }
+}
